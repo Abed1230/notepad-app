972703c369e63ac5113729c0f97436648a340fe3
Abed1230 <33055026+Abed1230@users.noreply.github.com>
2018-03-31 21:56:51 +0200

    created note class

---
+++ app/src/main/java/com/abed/notepad/Note.java
@@ -0,0 +1,51 @@
+package com.abed.notepad;
+
+/**
+ * Created by Abed on 03/31/2018.
+ */
+
+public class Note {
+    private String title;
+    private String text;
+    private String date;
+    private String tag;
+
+    public Note(String title, String text, String date, String tag) {
+        this.title = title;
+        this.text = text;
+        this.date = date;
+        this.tag = tag;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
