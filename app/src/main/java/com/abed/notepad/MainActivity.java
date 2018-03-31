fb8825f79e1cbc1656a2207bd44dbd985851e428
Abed1230 <33055026+Abed1230@users.noreply.github.com>
2018-03-31 22:54:13 +0200

    setup layout foundation for main activity

--- app/src/main/java/com/abed/notepad/MainActivity.java
+++ app/src/main/java/com/abed/notepad/MainActivity.java
@@ -2,6 +2,7 @@ package com.abed.notepad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView gv = findViewById(R.id.gridView);
+        NotesAdapter adapter = new NotesAdapter(this, null);
+        gv.setAdapter(adapter);
     }
 }
